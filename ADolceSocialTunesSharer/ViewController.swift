0bedc485d6ec5c4d64522c1d39c297afc03cd65d
Andrew Dolce <andrew@intrepid.io>
2015-11-14 11:51:42 -0500

    Initial Commit

---
+++ ADolceSocialTunesSharer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ADolceSocialTunesSharer
+//
+//  Created by Andrew Dolce on 11/14/15.
+//  Copyright © 2015 Intrepid Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
